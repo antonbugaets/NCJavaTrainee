64a5edb8f510b86a33d45b40452759f05ffdc87b
AntonSamSTU <newlife163rus@gmail.com>
2021-08-28 21:23:56 +0300

    Add test for commit

--- src/ua/edu/sumdu/j2se/Bugaetc/Task.java
+++ src/ua/edu/sumdu/j2se/Bugaetc/Task.java
@@ -8,21 +8,24 @@ public class Task {
     private int end;
     private int interval;
 
-    private boolean active = true;
+    private boolean active;
 
-    private boolean repeatable; //
+    private boolean repeatable;
 
-    Task(String title, int time) { //constructor constructs an inactive task to run at a specified time without repeating with a given name.
+    /**
+     * constructor constructs an inactive task to run at a specified time without repeating with a given name.
+     */
+    public Task(String title, int time) {
         this.title = title;
         this.time = time;
         repeatable = false;
     }
 
-    /*
-    constructor constructs an inactive task to run within the specified time range
-    (including the start and the end time) with the set repetition interval and with a given name.
+    /**
+     * constructor constructs an inactive task to run within the specified time range
+     * (including the start and the end time) with the set repetition interval and with a given name.
      */
-    Task(String title, int start, int end, int interval) {
+    public Task(String title, int start, int end, int interval) {
         this.title = title;
         this.start = start;
         this.end = end;
@@ -38,7 +41,7 @@ public class Task {
         this.title = title;
     }
 
-    Boolean isActive() {
+    boolean isActive() {
         return active;
     }
 
@@ -50,27 +53,38 @@ public class Task {
         return repeatable;
     }
 
-    //Methods for reading and changing execution time for non-repetitive tasks:
+    /**Methods for reading and changing execution time for non-repetitive tasks:v*/
 
-    int getTime() { //if the task is a repetitive one, the method must return the start time of the repetition;
+    /**
+     * if the task is a repetitive one, the method must return the start time of the repetition;
+     */
+    int getTime() {
         if (!isRepeated()) {
             return time;
         } else return start;
 
     }
 
-    void setTime(int time) { // if the task was a repetitive one, it should become non-repetitive.
+    /**
+     * if the task was a repetitive one, it should become non-repetitive.
+     */
+
+    void setTime(int time) {
+        this.time = time;
         if (isRepeated()) {
-            this.time = time;
-            this.start = 0;
-            this.end = 0;
-            this.interval = 0;
+            this.start = Integer.parseInt(null);
+            this.end = Integer.parseInt(null);
+            this.interval = Integer.parseInt(null);
             repeatable = false;
-        } else throw new RepeatableTaskEx("It's already non repeatable  task");
+        }
     }
 
-    // Methods for reading and changing execution time for repetitive tasks:
-    int getStartTime() { //if the task is a non-repetitive one, the method must return the time of the execution;
+    /** Methods for reading and changing execution time for repetitive tasks:*/
+
+    /**
+     * if the task is a non-repetitive one, the method must return the time of the execution;
+     */
+    int getStartTime() {
         if (isRepeated()) {
             return start;
         } else return time;
@@ -88,23 +102,31 @@ public class Task {
         } else return 0;
     }
 
-    void setTime(int start, int end, int interval) { //if the task is a non-repetitive one, it should become repetitive.
+    /**
+     * if the task is a non-repetitive one, it should become repetitive.
+     */
+
+    void setTime(int start, int end, int interval) {
+        this.start = start;
+        this.end = end;
+        this.interval = interval;
         if (!isRepeated()) {
-            this.start = start;
-            this.end = end;
-            this.interval = interval;
-            this.time = 0;
+            this.time = Integer.parseInt(null);
             repeatable = true;
-        } else throw new RepeatableTaskEx("It's already repeatable  task");
+        }
     }
 
-    /*
-    You should add the int nextTimeAfter (int current)
-    method to the Task class that returns the next start time of the task execution after the current time.
+    /**
+     You should add the int nextTimeAfter (int current)
+     method to the Task class that returns the next start time of the task execution after the current time.
      If after the specified time the task is not executed anymore, the method must return -1.
      */
-    int nextTimeAfter(int current) { // current - time at this moment
-        //for non repeatable task:
+
+    /**
+     * current - time at this moment
+     */
+    int nextTimeAfter(int current) {
+        /**for non repeatable task:*/
         if (!isRepeated()) {
             if (time < current) {
                 //active =false;
@@ -113,28 +135,30 @@ public class Task {
                 return time;
             }
         }
-        //for  repeatable task:
+        /**for  repeatable task:*/
         if (current > end) {
             return -1;
         }
-        //helpful variable's
-        int count = start; //last Iteration with current
-        int lastIteration = start; //Last Iteration w/o current
-
-        while (lastIteration <= end) { ////calculation last Iteration w/o current
+        /**helpful variable's*/
+        /**last Iteration with current*/
+        int count = start;
+        /**Last Iteration w/o current*/
+        int lastIteration = start;
+        /** calculation last Iteration w/o current*/
+        while (lastIteration <= end) {
             lastIteration += interval;
         }
         lastIteration -= interval;
-
-        if (current > lastIteration) { //the task will not be executed anymore
+        /**the task will not be executed anymore*/
+        if (current > lastIteration) {
             return -1;
         }
-
-        while (count < current) { //calculation last Iteration with current
+        /**calculation last Iteration with current*/
+        while (count < current) {
             count += interval;
         }
-        //  System.out.println("Last i: " + lastIteration);
-        return count; // return last Iteration with current
+        /** return last Iteration with current*/
+        return count;
     }
 }
 
