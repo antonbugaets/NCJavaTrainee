4634b992d85195db059c5fffbcdea6cf6ef28e32
AntonSamSTU <newlife163rus@gmail.com>
2021-10-04 18:33:39 +0300

    Add test for commit

--- src/ua/sumdu/j2se/Anton/tasks/TaskIO.java
+++ src/ua/sumdu/j2se/Anton/tasks/TaskIO.java
@@ -50,8 +50,72 @@ public class TaskIO {
     /**
      * reads tasks from the stream to the current task list.
      */
-
     private static void read(AbstractTaskList tasks, InputStream in) throws IOException, ClassNotFoundException {
+        DataInputStream dataInputStream = new DataInputStream(in);
+        int numOfTasks = dataInputStream.readInt();
+        for (int i = 0; i < numOfTasks; i++) {
+            Task value;
+            //skip one value
+            int titleLength = dataInputStream.readInt();
+            //read title
+            String title = dataInputStream.readUTF();
+            //read activity
+            int isActive = dataInputStream.readInt();
+            //set activity in boolean
+            boolean activity = false;
+            if (isActive == 1) {
+                activity = true;
+            }
+
+            Period interval = null;
+            //read interval in seconds
+            long intervalInSec = dataInputStream.readLong();
+            //set interval in Period
+            if (intervalInSec > 0) {
+                interval = Period.ofDays((int) (intervalInSec / 86400));
+            }
+
+            //create task
+            if (interval == null) {
+                value = new Task(title, LocalDateTime.ofInstant(Instant.ofEpochSecond(dataInputStream.readLong()), ZoneId.systemDefault()));
+            } else {
+
+                value = new Task(title,
+                        LocalDateTime.ofInstant(Instant.ofEpochSecond(dataInputStream.readLong()), ZoneId.systemDefault()),
+                        LocalDateTime.ofInstant(Instant.ofEpochSecond(dataInputStream.readLong()), ZoneId.systemDefault()),
+                        interval);
+            }
+            //set activity in task
+            value.setActive(activity);
+            //add task in list
+            tasks.add(value);
+
+
+        }
+    }
+
+
+    /**
+     * writes tasks from the list to the file.
+     */
+    public static void writeBinary(AbstractTaskList tasks, File file) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        write(tasks, fileOutputStream);
+    }
+
+    /**
+     * reads tasks from the file to the task list.
+     */
+
+    public static void readBinary(AbstractTaskList tasks, File file) throws IOException, ClassNotFoundException {
+        FileInputStream in = new FileInputStream(file);
+        read(tasks, in);
+    }
+
+
+
+/*
+    private static void readOr(AbstractTaskList tasks, InputStream in) throws IOException, ClassNotFoundException {
         DataInputStream dataInputStream = new DataInputStream(in);
         int numOfTasks = dataInputStream.readInt();
         for (int i = 0; i < numOfTasks; i++) {
@@ -88,20 +152,6 @@ public class TaskIO {
         }
     }
 
-    /**
-     * writes tasks from the list to the file.
-     */
-    public static void writeBinary(AbstractTaskList tasks, File file) throws IOException {
-        FileOutputStream fileOutputStream = new FileOutputStream(file);
-        write(tasks, fileOutputStream);
-    }
+ */
 
-    /**
-     * reads tasks from the file to the task list.
-     */
-
-    public static void readBinary(AbstractTaskList tasks, File file) throws IOException, ClassNotFoundException {
-        FileInputStream in = new FileInputStream(file);
-        read(tasks, in);
-    }
 }
